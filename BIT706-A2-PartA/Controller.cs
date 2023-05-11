aef72721d14c9c66d6b796c9f5aef38503c8599a
Hayden <105952425+Hayden2147483647@users.noreply.github.com>
2023-05-11 14:35:11 +1200

    First Commit

---
+++ BIT706-A2-PartA/Controller.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIT706_A2_PartA
+{
+    internal class Controller
+    {
+        Customer customer;
+
+        public void addCustomer(int id, string name)
+        {
+            customer = new Customer(id, name);
+        }
+
+        public void removeCustomer(int id)
+        {
+            if (customer.getId() == id)
+            {
+                customer = null;
+            }
+            else
+            {
+                throw new Exception();
+            }
+        }
+
+        public void editCustomer(Customer cust)
+        {
+            cust.setId(customer.getId());
+            cust.setName(customer.getName());
+        }
+    }
+}
---
+++ BIT706-A2-PartA/Customer.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BIT706_A2_PartA
+{
+    internal class Customer
+    {
+        private int id;
+        private string name;
+
+        public Customer(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public int getId()
+        {
+            return id;
+        }
+
+        public string getName() 
+        { 
+            return name;
+        }
+
+        public void setId(int id)
+        {
+            this.id = id;
+        }
+
+        public void setName(string name)
+        {
+            this.name = name;
+        }
+    }
+}
